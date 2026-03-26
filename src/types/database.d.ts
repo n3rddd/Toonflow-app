110f2b03d8acef50e87afeb3bc07abb630c9c592
zhishi <1951671751@qq.com>
2026-03-26 11:37:13 +0800

    增加宫格图片下载

--- src/types/database.d.ts
+++ src/types/database.d.ts
@@ -1,6 +1,25 @@
-// @db-hash a3673cf3a1d1c9cbf22ae3cfff196a71
+// @db-hash 71b2e55243e59382321a140a8d9a64ff
 //该文件由脚本自动生成，请勿手动修改
 
+export interface _o_storyboard_old_20260325 {
+  'camera'?: string | null;
+  'createTime'?: number | null;
+  'description'?: string | null;
+  'duration'?: string | null;
+  'filePath'?: string | null;
+  'frameMode'?: string | null;
+  'id'?: number;
+  'lines'?: string | null;
+  'mode'?: string | null;
+  'model'?: string | null;
+  'prompt'?: string | null;
+  'reason'?: string | null;
+  'resolution'?: string | null;
+  'scriptId'?: number | null;
+  'sound'?: string | null;
+  'state'?: string | null;
+  'title'?: string | null;
+}
 export interface memories {
   'content': string;
   'createTime': number;
@@ -138,6 +157,7 @@ export interface o_storyboard {
   'filePath'?: string | null;
   'frameMode'?: string | null;
   'id'?: number;
+  'index'?: string | null;
   'lines'?: string | null;
   'mode'?: string | null;
   'model'?: string | null;
@@ -201,6 +221,7 @@ export interface o_videoConfig {
 }
 
 export interface DB {
+  "_o_storyboard_old_20260325": _o_storyboard_old_20260325;
   "memories": memories;
   "o_agentDeploy": o_agentDeploy;
   "o_agentWorkData": o_agentWorkData;
