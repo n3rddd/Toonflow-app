498c598a67a294fac706b9fab1fb5c7eed4b4ba5
zhishi <1951671751@qq.com>
2026-05-01 19:11:08 +0800

    数据库修复

--- src/lib/fixDB.ts
+++ src/lib/fixDB.ts
@@ -66,6 +66,8 @@ export default async (knex: Knex): Promise<void> => {
   // 添加新字段
   await addColumn("o_agentDeploy", "maxOutputTokens", "integer");
   await addColumn("o_assets", "audioBindState", "integer");
+  await addColumn("o_modelPrompt", "fileName", "string");
+  await addColumn("o_modelPrompt", "path", "string");
   const vendorDataSelect = await u.db("o_vendorConfig").whereIn("id", ["deepseek", "atlascloud"]).select("*");
   if (!vendorDataSelect.find((i) => i.id == "deepseek")) {
     await u.db("o_vendorConfig").insert({
